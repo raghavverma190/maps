da23eb3e14317c74183c02aa8bbaf46837721430
Raghav Verma <raghav.verma190@gmail.com>
2024-06-12 03:01:00 +0530

    Implementing google maps

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,11 @@ import { Company } from './Company';
 const user = new User();
 const company = new Company();
 console.log(user, company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
